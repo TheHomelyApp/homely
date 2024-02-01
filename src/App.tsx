956bedf362d150f33f7b40abc222bb2b2efaf734
marco.riolo <marco.riolo@codeland.it>
2024-02-01 17:08:08 +0100

    start implementing basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer/Footer"
+import Nav from "./components/Nav/Nav"
+
 function App() {
   return (
     <>
+    <Nav />
     <main>
-      <h1>Homely</h1>
     </main>
+    <Footer />
     </>
   )
 }
